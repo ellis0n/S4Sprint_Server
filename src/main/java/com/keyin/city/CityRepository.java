343852382fde57a52216c6af2241005cf5d21201
glen may <ellisonglenmay@gmail.com>
2023-02-20 15:19:15 -0330

    remove unneeded code

--- src/main/java/com/keyin/city/CityRepository.java
+++
@@ -1,9 +0,0 @@
-package com.keyin.city;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CityRepository
-        extends CrudRepository<City, Long> {
-}
