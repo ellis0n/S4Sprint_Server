d1ddc43151a82a9404e65a644e743d50f96fe564
glen may <ellisonglenmay@gmail.com>
2023-02-20 16:16:03 -0330

    added city class, fixed import statements in other classes

--- src/main/java/com/keyin/city/City.java
+++ src/main/java/com/keyin/city/City.java
@@ -1,5 +1,61 @@
 package com.keyin.city;
 
-public class City {
+import com.keyin.airport.Airport;
 
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+public class City {
+    @Id
+    @GeneratedValue(generator = "city_sequence")
+    @SequenceGenerator(name = "city_sequence", sequenceName = "city_sequence", allocationSize = 1, initialValue = 1)
+    private Long id;
+
+    private String name;
+    private String state;
+    private int population;
+
+    @OneToMany(mappedBy = "city")
+    private Set<Airport> airports;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public int getPopulation() {
+        return population;
+    }
+
+    public void setPopulation(int population) {
+        this.population = population;
+    }
+
+    public Set<Airport> getAirports() {
+        return airports;
+    }
+
+    public void setAirports(Set<Airport> airports) {
+        this.airports = airports;
+    }
 }
